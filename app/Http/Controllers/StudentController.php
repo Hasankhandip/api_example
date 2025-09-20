4f20766f5afa763b24b9d2b128e2f306bcad4cf6
Dip <hasankhandip@gmail.com>
2025-09-21 02:07:49 +0600

    #9 video

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,20 +3,33 @@ namespace App\Http\Controllers;
 
 use App\Models\Students;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class StudentController extends Controller {
     function list() {
         return Students::all();
     }
     function addStudent(Request $request) {
-        $student        = new Students();
-        $student->name  = $request->name;
-        $student->email = $request->email;
-        $student->phone = $request->phone;
-        if ($student->save()) {
-            return ["result" => "new student added"];
+        $rules = [
+            'name' => 'required | min:2 | max:10',
+            'email' => 'email | required',
+            'phone' => 'required',
+        ];
+        $validation = Validator::make($request->all(), $rules);
+
+        if ($validation->fails()) {
+            return $validation->errors();
         } else {
-            return ["result" => "new student not added"];
+
+            $student        = new Students();
+            $student->name  = $request->name;
+            $student->email = $request->email;
+            $student->phone = $request->phone;
+            if ($student->save()) {
+                return ["result" => "new student added"];
+            } else {
+                return ["result" => "new student not added"];
+            }
         }
     }
 
@@ -32,12 +45,21 @@ class StudentController extends Controller {
         }
     }
 
-    function deleteStudent($id){
-       $student=Students::destroy($id);
+    function deleteStudent($id) {
+        $student = Students::destroy($id);
         if ($student) {
             return ["result" => "student record deleted"];
         } else {
             return ["result" => "student record not deleted"];
+        }
+    }
+
+    function searchStudent($name) {
+        $student = Students::where('name', 'like', "%$name%")->get();
+        if ($student) {
+            return ["result" => $student];
+        } else {
+            return ["result" => "No record found"];
         }
     }
 }
